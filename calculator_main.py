4d9c256e9d9aed8068fd003faa1ace18bfa1942d
Kazanlu <98271626+Kazanlu@users.noreply.github.com>
2023-07-09 22:19:03 +0300

    Update calculator_main.py

--- calculator_main.py
+++ calculator_main.py
@@ -1,4 +1,18 @@
 class Calculator:
+    """
+       A simple calculator class for performing basic arithmetic operations.
+
+       Attributes:
+       - memory: Holds the current value in memory.
+
+       Methods:
+       - addition: Adds a value to the memory.
+       - subtraction: Subtracts a value from the memory.
+       - multiplication: Multiplies the memory by a value.
+       - division: Divide the memory by a value.
+       - root: Take (n) root of a memory.
+       """
+
     def __init__(self):
         self.memory = 0  # Current calculator number is zero as none of the actions was done yet.
 
@@ -12,18 +26,21 @@ class Calculator:
 
     # Method which will perform multiplication on current memory by number defined
     def multiplication(self, number):
-        self.memory *= number
+        self.memory *= float(number)
 
     # Method which will divide current memory by number defined
     def division(self, number):
         if number == 0:
-            print("It is not possible to divide by 0")
+            raise ValueError("Division was not done as input is not valid")
         else:
             self.memory /= number
 
     # Method which will take (n) root of a number where n = number
     def root(self, number):
-        self.memory **= 1 / number
+        if number == 0:
+            raise ValueError("Root was not taken as input is not valid")
+        else:
+            self.memory **= 1 / number
 
     # Method which will check current calculator memory
     def get_memory(self):
