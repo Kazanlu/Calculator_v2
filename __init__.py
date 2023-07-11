0124dcd5335fc5e42abc24bdb76b1245c728b471
Kazanlu <98271626+Kazanlu@users.noreply.github.com>
2023-07-11 13:22:52 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,14 @@
+"""
+       A simple calculator class for performing basic arithmetic operations.
+
+       Attributes:
+       - memory: Holds the current value in memory.
+
+       Methods:
+       - addition: Adds a value to the memory.
+       - subtraction: Subtracts a value from the memory.
+       - multiplication: Multiplies the memory by a value.
+       - division: Divide the memory by a value.
+       - root: Take (n) root of a memory.
+"""
+__version__ = '1.1.0'
